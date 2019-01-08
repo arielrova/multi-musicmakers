06e9e7710e939057ea7f999d0ac37304ba437526
vicgus <victor.gsson@gmail.com>
2019-01-08 16:22:32 +0100

    Updated Instruments

--- src/funkystuff/instrument2.js
+++ src/funkystuff/instrument2.js
@@ -1,28 +1,31 @@
 exports.createSynthesizer = function(Tone) {
     var chorus = new Tone.Chorus({
-            frequency  : 1.5 ,
-            delayTime  : 3.5 ,
+            frequency  : 3 ,
+            delayTime  : 2 ,
             depth  : 0.9 ,
             type  : 'sine',
             spread  : 180            
     }).toMaster();
 
+    var filter = new Tone.Filter(400, "bandpass", -24).toMaster();
+    
     var synth = new Tone.Synth({
         oscillator : {
-            type : 'triangle8',
-            modulationType: 'sine',
+            type : 'square',
+            modulationType: 'square',
             modulationIndex: 3,
             harmonicity: 3
       },
         envelope : {
-            attack : 0.2,
-            decay : 0.1,
+            attack : 0.01,
+            decay : 0.4,
             sustain: 0.2,
-            release: 0.5
-      }
-    }).connect(chorus);
+            release: 0.3
+      },
+      volume: -10
+    });
 
-    var synthesizer = synth.toMaster()
+    var synthesizer = synth.chain(filter, chorus, Tone.Master);
 
     return synthesizer
 }
